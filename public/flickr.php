d8975a1a5c598412a9c12f15535b27ac44db5800
Nyall <nyall.mccurrach@rye.agency>
2017-10-16 19:30:05 +0100

    If there is a curl error, or invalid http response, or no results - pass the relevant error information in the json object

--- public/flickr.php
+++ public/flickr.php
@@ -1,10 +1,13 @@
 <?php
 
   // Empty array to begin constructing object which will be encoded and sent to front end
-  $data_array = array();
+  $response = [];
+  $response['error'] = null;
+  $response['status'] = 1;
+  $response['data'] = [];
 
   // tags are formatted into uri safe string before the getJson request is made in JS
-  $tags = $_GET['tagUri'];
+  $tags = urlencode($_GET['tagUri']);
   // query string
   $query = 'https://api.flickr.com/services/feeds/photos_public.gne?tagmode=all&format=json&nojsoncallback=1&tags='.$tags;
 
@@ -22,6 +25,8 @@
   //Get any error info
   $errno = curl_errno($ch);
   $error = curl_error($ch);
+  $response['error'] = $error;
+
   $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
   //echo $data . "<br>";
   //echo $errno . "<br>";
@@ -33,6 +38,8 @@
     $jsonDecoded = json_decode( $data ); // stdClass object
 
     if ( $jsonDecoded->items && count($jsonDecoded->items) > 0 ) {
+      $response['error'] = null;
+      $response['status'] = 1;
       foreach ( $jsonDecoded->items as $item ) {
         // Construct an object containing relevant information to be returned on the web page, no need to include everything that isn't being used. By converting it into these objects instead of passing through the exact response I could add inputs from other sources that aren't just the flickr api.
         $item_details = array(
@@ -41,16 +48,22 @@
           "imgSrc" => $item->media->m,
           "tags" => $item->tags
         );
-        $data_array[] = $item_details;
+        $response['data'][] = $item_details;
       }
-      $finalJson = json_encode($data_array, JSON_FORCE_OBJECT);
-      echo $finalJson;
-      return $finalJson;
-    } else {
-      return json_encode(array("Error"=>"999"));
-    }
 
+    } else {
+      $response['error'] = "No results";
+      $response['status'] = 0;
+
+    }
+  } else {
+    $response['error'] = "Not a valid HTTP response (".$resultStatus.")";
+    $response['status'] = 0;
   }
+  $finalJson = json_encode($response, JSON_FORCE_OBJECT);
+  echo $finalJson;
+  return $finalJson;
+
 
 
   //If using php_serial format unserialize the data and all info is in resulting array
